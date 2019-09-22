c453865384092dc9f24b916bb5aef9d260c3ef0a
dala1207@naver.com <dala1207@naver.com>
2019-09-22 23:57:54 +0900

    commit

--- P010_SNS_nodebird/models/user.js
+++ P010_SNS_nodebird/models/user.js
@@ -1,10 +1,52 @@
-module.exports = (sequelize, DataTypes)=>{
-
+module.exports = (sequelize, DataType) => (
 
     sequelize.define('birduser',{
 
+            userid: {
+
+                type:DataType.STRING(20), 
+                primaryKey:true,
+                
+
+            },                  
+
+            email: {
+
+                
+                type:DataType.STRING(40), 
+                primaryKey:true,
+
+            }, 
+
+            nick: {
+
+                type: DataType.STRING(15),
+                allowNull: false,
+            },
+
+            passwords:{
+
+                type: DataType.STRING(100),
+                allowNull: false,
+            },
+
+            providers:{
 
 
-    })
+                type: DataType.STRING(100),
+                allowNull: false,
+                defaultValue: 'local',
+            },
 
-}
+            snsId:{
+
+                type: DataType.STRING(30),
+                allowNull: true,
+            },
+        },
+            {
+
+                timestamps: true,
+                paranoid: true, 
+            })
+);
