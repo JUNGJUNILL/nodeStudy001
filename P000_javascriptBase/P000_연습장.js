3a995ec1ba576ea4e886c5d6ff84a17a057ca762
JUNGJUNILL <dala1207@naver.com>
2020-03-20 18:01:48 +0900

    commit

--- P000_javascriptBase/P000_연습장.js
+++ P000_javascriptBase/P000_연습장.js
@@ -1,18 +1,22 @@
-const aaa ="정준일"; 
-const bbb ="정준이";
 
-const switchFunc = (param)=>{
-
-    switch(param){
-
-        case aaa: return '정주닐'; 
-        case bbb: return '주닐정'; 
-        break; 
-        default : return '헬로우'; 
-
-    }
+const todos =  [
+  {id:0, text: '  리액트 소개', checked: false},
+  {id:1, text: '  리액트 소개', checked: true},
+  {id:2, text: '  리액트 소개', checked: false},
+] ;
 
 
-}
+const add = [...todos,{id:3, text: ' ' , checked:true}]
 
-console.log(switchFunc("ㅇ")); 
+//add.map((v)=>console.log(v,'헬로우')); 
+
+
+
+const newArray = todos.concat({id:4,text:'씌발' , checked: true});
+// newArray.map(v=>console.log(v));
+
+const newArray01 = [...todos,{id:5, text:'신발', checked: true}]
+newArray01.map(v=>console.log(v));
+
+
+newArray01.findIndex(todo=>todo.id)
