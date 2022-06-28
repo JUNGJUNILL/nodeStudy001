40014e7588ea07003be997edfae01e3b553dfb49
JUNGJUNILL <dala1207@naver.com>
2022-06-28 17:00:01 +0900

    commit

--- P000_makeinsertQuyery/hubmeka.js
+++ P000_makeinsertQuyery/hubmeka.js
@@ -1,18 +1,25 @@
 var fs = require('fs');
-const result = `'입고일자','매출합계','매출면세','매출과세','매출부가세'`;
+const result = `'일자','계정','품목명','규격','비고','수량','단가','매출_출금','매입_입금','구분','잔액'`;
 
-
+const resultLength = result.split(',').length; 
+let str ='\n'; 
+console.log(resultLength); 
 result.split(',').map((v,i)=>{
 
     
 // fs.writeFile(__dirname+'/test.txt', v, 'utf8', function(error){ console.log('write end') });
 
+if(result.length-1==i){
+    v=v.replace(/\'|\"/gi, ""); 
+}else{
+    v=v.replace(/\'|\"/gi, "")+'\n'; 
+}
+
 try{
-    
-    fs.appendFileSync(__dirname+'/hubmke_array.txt', v.replace(/\'/gi, "")+'\n');
-   if(result.length-1 === i){
-       console.log('파일 생성이 완료되었습니다.'); 
-   }
+
+    fs.appendFileSync(__dirname+'/hubmke_array.txt', v);
+    console.log(i); 
+
 
 }catch(error){
 console.error(error); 
